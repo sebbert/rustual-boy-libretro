b9f4d9153aa6652bf17ae30060fa7d524d9274c9
Sebastian Reinhard <sebastian@sebbert.me>
2017-02-06 15:50:05 +0100

    SystemInfo completeness

--- src/system_info.rs
+++ src/system_info.rs
@@ -12,6 +12,8 @@ pub struct SystemInfo {
     pub library_name: *const i8,
     pub library_version: *const i8,
     pub valid_extensions: *const i8,
+    pub need_fullpath: bool,
+    pub block_extract: bool
 }
 
 impl SystemInfo {
@@ -19,7 +21,9 @@ impl SystemInfo {
 		SystemInfo {
 			library_name: cstring!("Rustual Boy"),
 			library_version: cstring!(env!("CARGO_PKG_VERSION")),
-			valid_extensions: cstring!("vb")
+			valid_extensions: cstring!("vb"),
+			need_fullpath: false,
+			block_extract: false
 		}
 	}
 }
