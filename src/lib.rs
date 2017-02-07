fb810a4b947e755e30d1d668264a1ac58c4dce77
Sebastian Reinhard <sebastian@sebbert.me>
2017-02-08 00:06:38 +0100

    Use correct display resolution instead of VRAM framebuffer resolution
    See https://github.com/emu-rs/rustual-boy/pull/15#issuecomment-278159465

--- src/lib.rs
+++ src/lib.rs
@@ -29,10 +29,10 @@ impl Context {
 	pub fn system_av_info(&self) -> SystemAvInfo {
 		SystemAvInfo {
 			geometry: SystemGameGeometry {
-				base_width: vip::FRAMEBUFFER_RESOLUTION_X as u32,
-				base_height: vip::FRAMEBUFFER_RESOLUTION_Y as u32,
-				max_width: vip::FRAMEBUFFER_RESOLUTION_X as u32,
-				max_height: vip::FRAMEBUFFER_RESOLUTION_Y as u32,
+				base_width: vip::DISPLAY_RESOLUTION_X as u32,
+				base_height: vip::DISPLAY_RESOLUTION_Y as u32,
+				max_width: vip::DISPLAY_RESOLUTION_X as u32,
+				max_height: vip::DISPLAY_RESOLUTION_Y as u32,
 
 				// Optional
 				aspect_ratio: 0.0
