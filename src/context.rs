bd9f9d71bad20cd8239468343f73f2469424be71
Sebastian Reinhard <sebastian@sebbert.me>
2017-02-13 10:04:25 +0100

    Only output most recent frame

--- src/context.rs
+++ src/context.rs
@@ -5,12 +5,14 @@ use rustual_boy_core::virtual_boy::VirtualBoy;
 use rustual_boy_core::rom::Rom;
 use rustual_boy_core::sram::Sram;
 use rustual_boy_core::time_source::TimeSource;
+use rustual_boy_core::sinks::{ Sink, VideoFrame };
 
 extern crate rustual_boy_middleware;
 use rustual_boy_middleware::{
 	Color,
 	Anaglyphizer,
-	GammaAdjustSink
+	GammaAdjustSink,
+	MostRecentSink
 };
 
 use retro_time_source::RetroTimeSource;
@@ -60,25 +62,33 @@ impl Context {
 	}
 
 	pub fn run_frame(&mut self, callbacks: &'static Callbacks) {
+		let mut most_recent_sink: MostRecentSink<VideoFrame> = MostRecentSink::new();
 		let mut audio_output_sink = CallbackSink(callbacks);
-		let video_output_sink = CallbackSink(callbacks);
-
-		let gamma_adjust = GammaAdjustSink::new(video_output_sink, 2.2);
-
-		let mut anaglyphizer = Anaglyphizer::new(
-			gamma_adjust,
-			Color::from((255, 0, 100)),
-			Color::from((0, 255, 255))
-		);
 
 		// TODO: Record initial time and take difference
 		let target_emulated_time_ns = self.time_source.time_ns();
 		let target_emulated_cycles = target_emulated_time_ns / 50;
 
 		while self.emulated_cycles < target_emulated_cycles {
-			let (num_cycles, _) = self.virtual_boy.step(&mut anaglyphizer, &mut audio_output_sink);
+			let (num_cycles, _) = self.virtual_boy.step(&mut most_recent_sink, &mut audio_output_sink);
 
 			self.emulated_cycles += num_cycles as _;
+		}
+
+		if most_recent_sink.has_frame() {
+			let video_output_sink = CallbackSink(callbacks);
+
+			let gamma_adjust = GammaAdjustSink::new(video_output_sink, 2.2);
+
+			let mut anaglyphizer = Anaglyphizer::new(
+				gamma_adjust,
+				Color::from((255, 0, 100)),
+				Color::from((0, 255, 255))
+			);
+
+			let top_sink = &mut anaglyphizer as &mut Sink<VideoFrame>;
+
+			top_sink.append(most_recent_sink.into_inner().unwrap())
 		}
 	}
 }
