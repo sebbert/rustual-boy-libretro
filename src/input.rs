485f49fe2de09c74bc5e372fadc0f3779859f6a7
Sebastian Reinhard <sebastian@sebbert.me>
2017-02-15 19:36:19 +0100

    Implement analog input

--- src/input.rs
+++ src/input.rs
@@ -22,6 +22,19 @@ pub enum JoypadButton {
 	R3 = 15
 }
 
+#[derive(Clone)]
+pub enum AnalogAxis {
+	X = 0,
+	Y = 1
+}
+
+#[derive(Clone)]
+pub enum AnalogStick {
+	Left = 0,
+	Right = 1
+}
+
+
 #[allow(dead_code)]
 pub enum RetroDeviceType {
 	None = 0,
@@ -36,5 +49,12 @@ pub enum RetroDeviceType {
 impl Callbacks {
 	pub fn joypad_button(&self, button: JoypadButton) -> bool {
 		0 != self.input_state(0, RetroDeviceType::Joypad as u32, 0, button as u32)
+	}
+
+	pub fn analog_xy(&self, stick: AnalogStick) -> (i16, i16) {
+		let x = self.input_state(0, RetroDeviceType::Analog as u32, stick.clone() as u32, AnalogAxis::X as u32);
+		let y = self.input_state(0, RetroDeviceType::Analog as u32, stick as u32, AnalogAxis::Y as u32);
+
+		(x, y)
 	}
 }
